536110b8d1a4655b2630a7db151d82b734946d75
Geetesh Sood <76897516+Geeteshsood@users.noreply.github.com>
2025-02-09 15:16:02 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,62 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use Parcel to bundle this sandbox, you can find more info about Parcel
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+
+const pr1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("p1");
+  }, 1000);
+});
+const pr2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject("p2");
+  }, 2000);
+});
+const pr3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("p3");
+  }, 4000);
+});
+
+// Promise.all([pr1, pr2, pr3])
+//   .then((result) => {
+//     console.log("result", result);
+//   })
+//   .catch(() => {
+//     console.log("error");
+//   });
+
+const myPromise = function (taskList) {
+  let promiseCompleted = 0;
+  let results = [];
+
+  return new Promise((resolve, reject) => {
+    taskList.forEach((task, index) => {
+      task
+        .then((val) => {
+          promiseCompleted++;
+          results[index] = val;
+          if (promiseCompleted == taskList.length) {
+            resolve(results);
+          }
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  });
+};
+
+myPromise([pr1, pr2, pr3])
+  .then((result) => {
+    console.log("result", result);
+  })
+  .catch((error) => {
+    console.log("error", error);
+  });
